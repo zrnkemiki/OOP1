6b76784cea3a1e6f78116a6a319d78e24e2dcef0
zrnkemiki <zrnkemiki@gmail.com>
2018-09-03 21:36:29 +0200

    main sredjen

--- OOP_MZ/src/main/Main.java
+++ OOP_MZ/src/main/Main.java
@@ -38,14 +38,14 @@ public class Main {
 //		TipSobeIO.dodavanjeTipaSobe();
 //		TipSobeIO.azurirajTipSobe();	
 		
-//		Korisnik miso = logovanje();
-//		System.out.println("________________________________________________");
-//		System.out.println(miso.toString());
+		Korisnik miso = Meni.logovanje();
+		System.out.println("________________________________________________");
+		System.out.println(miso.toString());
 		
-		System.out.println("Cenovnik");
+//		System.out.println("Cenovnik");
 //		CenovnikIO.dodavanjeCenovnik();
-		CenovnikIO.brisanjeCenovnika();
-		CenovnikIO.azurirajCenovnik();
+//		CenovnikIO.brisanjeCenovnika();
+//		CenovnikIO.azurirajCenovnik();
 		
 //		System.out.println("Izmena korisnika: ");
 //		KorisnikIO.izmenaKorisnika();
@@ -92,60 +92,6 @@ public class Main {
 		// System.out.println(DataBase.sobe.keySet());
 	}
 	
-	public static Korisnik logovanje() {
-		Korisnik temp = new Korisnik();
-		@SuppressWarnings("resource")
-		Scanner input = new Scanner(System.in);
-		
-		String korisnickoIme;
-		String lozinka;
-		boolean aktivan;
-		
-		while(true) {
-			aktivan = true;
-			System.out.println("Unesite korisnicko ime");
-			korisnickoIme = input.nextLine();
-			if (korisnickoIme == "") {
-				System.out.println("Korisnicko ime ne moze biti prazno polje.");
-				continue;
-			}
-			System.out.println("unesite lozinku");
-			lozinka = input.nextLine();
-			
-			if (lozinka == "") {
-				System.out.println("Lozinka ne moze biti prazno polje.");
-				continue;
-			}
-			
-			
-			//__________________ADMIN LOGIN____________
-			if(korisnickoIme.equals("admin") && lozinka.equals("admin")) {
-				System.out.println("adminske privilegije");
-				aktivan = false;
-			}
-			//______________USERS LoGIN___________________
-			for(String key : DataBase.korisnici.keySet()) {
-				if(DataBase.korisnici.get(key).getKorisnickoIme().equals(korisnickoIme) && DataBase.korisnici.get(key).getLozinka().equals(lozinka)) {
-					if(!DataBase.korisnici.get(key).isAktivnost()) {
-						System.out.println("Podaci koje ste uneli su ispravni ali korisnik nije aktivan u sistemu.");
-						aktivan = false;
-					}
-					else {
-						System.out.println("Uspesno ste se logovali kao" + DataBase.korisnici.get(key).getIme());
-						temp = DataBase.korisnici.get(key);
-						aktivan = false;
-						return temp;
-					}				
-				}
-			}	
-			
-			if(aktivan) {
-				System.out.println("Korisnicko ime/Lozinka nisu dobri.");
-			}
-			else
-				continue;				
-		}
-				
-	}
+	
 
 }
